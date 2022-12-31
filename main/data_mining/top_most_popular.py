df3b9ed4092e967c55851b6a99b6280d80fd6745
daniel <danieldfgro2000@gmail.com>
2022-12-31 20:09:35 +0200

    data mining - average price

--- main/data_mining/top_most_popular.py
+++ main/data_mining/top_most_popular.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker
+import numpy as np
 import seaborn as sns
 
 from main.data_mining.a_return_data_from_all_files import return_data_from_all_files, TimeFrameAndModelEnum
@@ -11,6 +12,7 @@ sns.set(rc={'axes.facecolor': 'cornflowerblue'})
 def show_plot():
 	ax = plt.gca()
 	ax.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(500))
+	ax.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(1))
 	plt.show()
 
 
@@ -34,12 +36,13 @@ def show_most_popular():
 
 def show_most_popular_by_year():
 	moto_count = all_moto_list.groupby(['Year', 'Price', 'Model'])
-	model_count = moto_count.Model.value_counts().iloc[:20]
-	# model_nr = count(moto_count.Model)
+
 	print(moto_count.size())
 	
 	sns.catplot(
-		data=all_moto_list.groupby("Model").filter(lambda x: len(x) > 30),
+		data=all_moto_list
+		.sort_values('Model')
+		.groupby("Model").filter(lambda x: len(x) > 30),
 		x='Year',
 		y='Model',
 		kind='box'
@@ -65,16 +68,34 @@ def show_most_popular_by_price_and_year():
 		data=all_moto_list
 		.sort_values('Model')
 		.groupby('Model')
-		.filter(lambda x: len(x) > 40),
-		x='Model',
+		.filter(lambda x: len(x) > 30),
+		x='Price',
 		y='Year',
-		size='Price',
-		sizes=(5, 500)
+		col='Model',
+		col_wrap=5,
+		# hue='Price',
+		# style='choice',
+		# kind='line',
+		dashes=False,
+		markers=True
+	
 	)
 	plt.show()
+	
+
+def show_price_estimation(query_model):
+	sns.lmplot(
+		data=all_moto_list.query(f'Model == "{query_model}"'),
+		x='Price',
+		y='Year',
+		ci=None,
+		scatter_kws={'s': 80}
+	)
+	show_plot()
 
 
 # show_most_popular()
 # show_most_popular_by_year()
 # show_most_popular_by_price()
-show_most_popular_by_price_and_year()
+# show_most_popular_by_price_and_year()
+show_price_estimation('z750')
